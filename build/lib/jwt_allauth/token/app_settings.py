51cd0b8475b01fd38a0dec1811a8ce6a682d475a
Fernando Castellanos <fcastellanos.dev@gmail.com>
2025-04-05 13:34:16 +0200

    Delete build directory

--- build/lib/jwt_allauth/token/app_settings.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-
-from jwt_allauth.token.tokens import RefreshToken as DefaultRefreshToken
-from jwt_allauth.utils import import_callable
-
-RefreshToken = import_callable(getattr(settings, 'JWT_ALLAUTH_REFRESH_TOKEN', DefaultRefreshToken))
